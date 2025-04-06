b41f740e8f70d5f71e5b0ed1ce8d2145b48c2f9c
Max <max.kvelland@gmail.com>
2025-04-06 15:45:40 +0200

    Fixed summertime

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("July 5, 2025 16:00:00").getTime();
+var countDownDate = new Date("July 5, 2025 15:00:00").getTime();
 
 // Update the countdown every 1 second
 var x = setInterval(function () {
